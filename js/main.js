f2c3739e28632510b1bb081aa6f3b58126fe2735
kartiksinghraghav2-netizen <kartiksinghraghav2@gmail.com>
2026-06-03 12:34:58 -0700

    Add form submission handler with alert and reset

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+document.getElementById("demoForm").addEventListener("submit", function (e) {
+  e.preventDefault();
 
+  alert(
+    "Thank you! Your demo request has been submitted. We will contact you shortly."
+  );
+
+  this.reset();
+});
